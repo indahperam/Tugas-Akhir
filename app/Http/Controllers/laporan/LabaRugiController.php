c185920a156e623aa6fd40a5e591ca36e9ed2d27
Joshua Sitompul <joshuasitompul2000@gmail.com>
2023-03-16 22:32:02 +0700

    +laba kotor

--- app/Http/Controllers/laporan/LabaRugiController.php
+++ app/Http/Controllers/laporan/LabaRugiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\laporan;
 use App\Http\Controllers\Controller;
 use App\Models\Pembelian;
 use App\Models\Pengeluaran;
+use App\Models\Produk;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
@@ -17,10 +18,21 @@ class LabaRugiController extends Controller
      */
     public function index()
     {
-        $transkasi = Transaksi::whereMonth('created_at', date('m'))->get(['total', 'diskon_total', 'ppn_total']);
+        $transkasi = Transaksi::with('transaksi_detail')->whereMonth('created_at', date('m'))->get(['id','total', 'diskon_total', 'ppn_total']);
         $penjualan_kotor = $transkasi->sum('total');
         $diskon_penjualan = $transkasi->sum('diskon_total');
         $penjualan_bersih = $penjualan_kotor - $diskon_penjualan;
+        $penjualan_pokok_data = $transkasi->map(function($q) {
+            $harga_pokok = 0;
+            foreach ($q->transaksi_detail as $key => $value) {
+                $produk = Produk::where('nama',$value->nama)->first()->harga_beli;
+                $harga_pokok+= $produk;
+            }
+            return $harga_pokok;
+        });
+
+        $penjualan_pokok = $penjualan_pokok_data->sum();
+        $laba_kotor = $penjualan_bersih- $penjualan_pokok;
 
         $pengeluaran = Pengeluaran::whereMonth('created_at', date('m'))->get(['nominal']);
         $pembelian = Pembelian::whereMonth('created_at', date('m'))->get(['total']);
@@ -29,20 +41,31 @@ class LabaRugiController extends Controller
         $total_pembelian = $pembelian->sum('total');
         $total_beban = $pengeluaran_operasional + $total_pembelian;
 
-        $laba_sebelum_pajak = $penjualan_bersih - $total_beban;
+        $laba_sebelum_pajak = $laba_kotor - $total_beban;
         $pajak_penghasilan = $transkasi->sum('ppn_total');
 
         $laba_bersih = $laba_sebelum_pajak - $pajak_penghasilan;
-        $master = compact('penjualan_kotor', 'diskon_penjualan', 'penjualan_bersih', 'pengeluaran_operasional', 'total_pembelian', 'total_beban', 'laba_sebelum_pajak', 'pajak_penghasilan', 'laba_bersih');
+        $master = compact('laba_kotor','penjualan_pokok','penjualan_kotor', 'diskon_penjualan', 'penjualan_bersih', 'pengeluaran_operasional', 'total_pembelian', 'total_beban', 'laba_sebelum_pajak', 'pajak_penghasilan', 'laba_bersih');
         return inertia()->render('laporan/Laba_Rugi', ['master' => $master]);
     }
 
     public function print()
     {
-        $transkasi = Transaksi::whereMonth('created_at', date('m'))->get(['total', 'diskon_total', 'ppn_total']);
+        $transkasi = Transaksi::with('transaksi_detail')->whereMonth('created_at', date('m'))->get(['id','total', 'diskon_total', 'ppn_total']);
         $penjualan_kotor = $transkasi->sum('total');
         $diskon_penjualan = $transkasi->sum('diskon_total');
         $penjualan_bersih = $penjualan_kotor - $diskon_penjualan;
+        $penjualan_pokok_data = $transkasi->map(function($q) {
+            $harga_pokok = 0;
+            foreach ($q->transaksi_detail as $key => $value) {
+                $produk = Produk::where('nama',$value->nama)->first()->harga_beli;
+                $harga_pokok+= $produk;
+            }
+            return $harga_pokok;
+        });
+
+        $penjualan_pokok = $penjualan_pokok_data->sum();
+        $laba_kotor = $penjualan_bersih- $penjualan_pokok;
 
         $pengeluaran = Pengeluaran::whereMonth('created_at', date('m'))->get(['nominal']);
         $pembelian = Pembelian::whereMonth('created_at', date('m'))->get(['total']);
@@ -51,11 +74,11 @@ class LabaRugiController extends Controller
         $total_pembelian = $pembelian->sum('total');
         $total_beban = $pengeluaran_operasional + $total_pembelian;
 
-        $laba_sebelum_pajak = $penjualan_bersih - $total_beban;
+        $laba_sebelum_pajak = $laba_kotor - $total_beban;
         $pajak_penghasilan = $transkasi->sum('ppn_total');
 
         $laba_bersih = $laba_sebelum_pajak - $pajak_penghasilan;
-        $master = compact('penjualan_kotor', 'diskon_penjualan', 'penjualan_bersih', 'pengeluaran_operasional', 'total_pembelian', 'total_beban', 'laba_sebelum_pajak', 'pajak_penghasilan', 'laba_bersih');
+        $master = compact('laba_kotor','penjualan_pokok','penjualan_kotor', 'diskon_penjualan', 'penjualan_bersih', 'pengeluaran_operasional', 'total_pembelian', 'total_beban', 'laba_sebelum_pajak', 'pajak_penghasilan', 'laba_bersih');
         return inertia()->render('print/labaRugiPrint', ['master' => $master]);
     }
 
--- resources/js/Pages/print/labaRugiPrint.vue
+++ resources/js/Pages/print/labaRugiPrint.vue
@@ -1,6 +1,10 @@
 <template lang="">
     <div class="w-[21cm]">
         <div class="text-center font-bold text-xl uppercase my-4">Laporan Laba Rugi</div>
+        <div class="text-center mb-4 text-2xl font-bold capitalize">
+        {{waktu}}
+    </div>
+    </div>
     <div class="grid text-2xl font-bold">
         Pendapatan Penjualan
     </div>
@@ -16,6 +20,16 @@
         <div class="">Pendapatan Penjualan Bersih</div>
         <div class=""></div>
         <div class="">{{rupiah(master.penjualan_bersih)}}</div>
+    </div>
+    <div class="grid grid-cols-3 text-lg font-bold">
+        <div class="">Harga Pokok Penjualan</div>
+        <div class=""></div>
+        <div class="border-b-2 border-black">{{rupiah(master.penjualan_pokok)}}</div>
+    </div>
+    <div class="grid grid-cols-3 text-lg font-bold">
+        <div class="">Laba Kotor</div>
+        <div class=""></div>
+        <div class="">{{rupiah(master.laba_kotor)}}</div>
     </div>
 
     <div class="grid text-xl font-bold mt-4">
@@ -53,14 +67,125 @@
         <div class=""></div>
         <div class="">{{rupiah(master.laba_bersih)}}</div>
     </div>
-    </div>
 </template>
 <script>
 import LayoutMain from '@/Layouts/LayoutMain.vue';
 export default {
+    layout: LayoutMain,
     props: {
         master: Object,
     },
+    setup() {
+        const datetime = new Date()
+        const hari = namaHari(datetime.getDay())
+        const tanggal = datetime.getMonth()
+        const bulan = namaBulan(datetime.getMonth())
+        const tahun = datetime.getFullYear()
+        const waktu = `${hari}, ${tanggal} ${bulan} ${tahun}`
+
+        function namaBulan(params) {
+            switch (params) {
+                case 0:
+                    return 'januari'
+                    break;
+            
+                case 1:
+                    return 'februari'
+                    break;
+            
+                case 2:
+                    return 'maret'
+                    break;
+            
+                case 3:
+                    return 'april'
+                    break;
+            
+                case 4:
+                    return 'mei'
+                    break;
+            
+                case 5:
+                    return 'juni'
+                    break;
+            
+                case 6:
+                    return 'juli'
+                    break;
+            
+                case 7:
+                    return 'agustus'
+                    break;
+            
+                case 8:
+                    return 'september'
+                    break;
+            
+                case 9:
+                    return 'oktober'
+                    break;
+            
+                case 10:
+                    return 'november'
+                    break;
+            
+                case 11:
+                    return 'desember'
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        function namaHari(params){
+            switch (params) {
+                case 1:
+                    return 'senin'
+                    break;
+            
+                case 2:
+                    return 'selasa'
+                    break;
+            
+                case 3:
+                    return 'rabu'
+                    break;
+            
+                case 4:
+                    return 'kamis'
+                    break;
+            
+                case 5:
+                    return 'jumat'
+                    break;
+            
+                case 6:
+                    return 'sabtu'
+                    break;
+            
+                case 7:
+                    return 'minggu'
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        return {
+            waktu
+        }
+    },
+    methods : {
+        async print(){
+            let print = await window.open(route('labaRugi.print'))
+            await print.addEventListener("DOMContentLoaded", async () => {
+                print.window.onafterprint = (event) => {
+                    print.close();
+                };
+                print.window.print()
+            });
+        }
+    }
 }
 </script>
 <style lang="">
